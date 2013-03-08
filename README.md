120daec3af19a869be09af14043f1178f8acc561
marcusthierfelder <mthierfe@oflws204.dwd.de>
2013-03-08 15:39:07 +0100

    example and doc

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 evolve
 ======
 
-generic time integrator library
+generic time integrator library for PDEs
+till now there is euler, icn, rk4, rk45 (time step adjustment)
+
+all you need is to set the grid size (1D,2D,3D,...), add variables
+as you want into a list of variables and provide a function which
+calculates the righthandsides and pass it to the library. Then 
+you can integrate. An simple output routine for vtk is included.
+
+
+example
+=======
+advection_1d:
+    advection equation which outputs ascii files (can be ploted with ygraph)
--- example/advection_1d/main.go
+++ example/advection_1d/main.go
@@ -1,41 +1,79 @@
 package main
 
 import (
-    "fmt"
-    . "github.com/marcusthierfelder/evolve"
-//"reflect"
+	"fmt"
+	"math"
+	"os"
+
+	. "github.com/marcusthierfelder/evolve"
 )
 
+var (
+	outfile = os.Getenv("GOPATH") + "/src/out_adv.dat"
+)
 
+func rhs(grid *Grid, r, evl VarList) {
+	f := evl.GetVar(0)
+	df := r.GetVar(0)
+	nx, dx := grid.GetSize()
 
+	// simple symmetric stencil
+	for i := 1; i < nx[0]-1; i++ {
+		df[i] = -0.5 / dx[0] * (f[i+1] - f[i-1])
 
-func (grid *Grid) rhs(r, evl VarList) {
-    
+		//df[i] = -1./dx[0] * (f[i]-f[i-1])
+	}
+
+	// continius bc
+	df[0], df[nx[0]-1] = df[nx[0]-2], df[1]
 }
 
-
 func main() {
-    fmt.Println("")
+	fmt.Println("Start an advection:")
 
+	/* tell the library the domain and which variables do exist */
+	grid := CreateGrid([]int{51}, []float64{0.}, []float64{1. / 50.})
+	grid.AddVars([]string{"f"})
 
-    grid := CreateGrid([]int{11}, []float64{0.}, []float64{1.})
-    grid.AddVars([]string{"x","f"})
+	/* create a variable list which is evolved */
+	vl := grid.CreateVarlist()
+	vl.AddVars([]string{"f"})
 
+	/* initialize integrator */
+	grid.TimeInt_init(vl, "rk4", interface{}(rhs))
 
-    x := grid.GetVar("x")
+	/* initial data */
+	x := grid.GetVar("x")
+	f := grid.GetVar("f")
+	for i := 0; i < 51; i++ {
+		f[i] = math.Exp(-50. * (x[i] - 0.5) * (x[i] - 0.5))
+	}
 
-    f := grid.GetVar("x")
+	/* output file */
+	fo, err := os.Create(outfile)
+	if err != nil {
+		panic(err)
+	}
+	defer fo.Close()
 
-    for i:=0; i<11; i++ {
-        x[i] = 1.        
-    }
-    f[0] = 1.
+	/* time step */
+	dt := 0.01
+	ot := 10
+	for t := 0; t < 10000; t++ {
+		// output timestep
+		if t%ot == 0 {
+			fmt.Println("step", t, grid.GetTime())
 
-
-
-
-
-
-
+			line := fmt.Sprintf("Time = %2.6f\n", grid.GetTime())
+			fo.Write([]byte(line))
+			for i, _ := range f {
+				line = fmt.Sprintf("%2.6e %2.6e\n", x[i], f[i])
+				fo.Write([]byte(line))
+			}
+			fo.Write([]byte("\n"))
+		}
+		// integrade one timestep
+		grid.TimeInt(vl, &dt)
+	}
 
 }
